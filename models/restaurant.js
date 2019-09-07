0d39a095da8ad4ca5095670d461bfd9d79bae2f7
Ariel Huang <ariel92911@gmail.com>
2019-09-07 09:50:27 +0800

    Add Mongoose connection and create Restaurant model

---
+++ models/restaurant.js
@@ -0,0 +1,12 @@
+// ./models/restaurant.js
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const restaurantSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  }
+})
+
+module.exports = mongoose.model('Restaurant', restaurantSchema)
